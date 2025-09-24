e9786d50adaa4c0c3acb2f5d9123d1de08c96531
Edimar18 <mosquida.edimar.18@gmail.com>
2025-09-24 16:14:58 +0800

    added some code on data entry

--- app/(tabs)/data_entry.tsx
+++ app/(tabs)/data_entry.tsx
@@ -1,13 +1,15 @@
-import { Platform, StyleSheet , Text, View} from 'react-native';
+import { Platform, StyleSheet , Text, View, ScrollView} from 'react-native';
 import ParallaxScrollView from '@/components/parallax-scroll-view';
 import { ThemedText } from '@/components/themed-text';
 import { ThemedView } from '@/components/themed-view';
 import { Link } from 'expo-router';
 
+
+const TAB =  ['Revenue', 'Expenses', 'Delivery', 'Product', 'Customer']
 export default function DataEntry() {
   return (
     <View style={styles.container}>
-      <Text>DATA ENTRY</Text>
+      <Text style={styles.header}>DATA ENTRY</Text>
     </View>
   );
 }
@@ -19,4 +21,9 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     backgroundColor: '#ECF0F1'
   },
+  header: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#2C3E50'
+  }
 });
